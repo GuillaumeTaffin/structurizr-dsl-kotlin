0787f6c846691c77e23b19b37f73d32e9dce9ccc
GuillaumeTaffin <guillaume.taffinjob@gmail.com>
2024-01-31 19:26:54 -0500

    setup the coverage quality gate to be executed at the end of the test

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     alias(libs.plugins.jvm)
     `java-library`
+    jacoco
 }
 
 repositories {
@@ -37,5 +38,39 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach 
 }
 
 tasks.named<Test>("test") {
+    finalizedBy(
+        tasks.jacocoTestReport,
+        tasks.jacocoTestCoverageVerification
+    )
     useJUnitPlatform()
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required = false
+        csv.required = false
+    }
+    dependsOn(tasks.test)
+}
+
+tasks.jacocoTestCoverageVerification {
+    violationRules {
+        rule {
+            includes = listOf("*")
+            limit {
+                counter = "BRANCH"
+                value = "COVEREDRATIO"
+                minimum = "1.0".toBigDecimal()
+            }
+        }
+
+        rule {
+            includes = listOf("*")
+            limit {
+                counter = "LINE"
+                value = "COVEREDRATIO"
+                minimum = "1.0".toBigDecimal()
+            }
+        }
+    }
 }
